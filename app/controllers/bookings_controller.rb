288d4df80980b6111814b93a8106b87bf971b235
jesus russian <jeduardoruss@gmail.com>
2022-08-26 15:32:10 +0200

    updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,42 +1,36 @@
 class BookingsController < ApplicationController
+  before_action :set_dress, only: [:create, :edit, :destroy, :update]
   def index
     @bookings = Booking.all
   end
-
   def show
     @booking = Booking.find(params[:id])
   end
-
   def edit
     @booking = Booking.find(params[:id])
   end
-
   def update
     @booking = Bookings.find(params[:id])
     @booking.update(booking_params)
     # No need for app/views/restaurants/update.html.erb
     redirect_to booking_path(@booking)
   end
-
   def create
-    @booking = Booking.new(_params)
+    @booking = Booking.new(booking_params)
     @booking.dress = @dress
+    @booking.user = current_user
     @booking.save
-    redirect_to dress_path(@dress)
+    redirect_to bookings_path
   end
-
   def new
     # We need @restaurant in our `simple_form_for`
     @dress = Dress.find(params[:dress_id])
     @booking = Booking.new
   end
-
   private
-
   def set_dress
     @dress = Dress.find(params[:dress_id])
   end
-
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
