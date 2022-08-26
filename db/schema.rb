533631b2a1388ceec7d5bd053290709067002378
Valerie Schweitzer <vmschweitzer@gmail.com>
2022-08-26 14:52:04 +0200

    booking option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_120155) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_095836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_120155) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["dress_id"], name: "index_bookings_on_dress_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
